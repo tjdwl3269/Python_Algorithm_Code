5b86f5326654be391d79c38ecc05eca44c1d5d42
hanjiseong <55674646+tjdwl3269@users.noreply.github.com>
2021-07-30 15:28:09 +0900

    Update insertion_sort.py

--- Sorting/insertion_sort.py
+++ Sorting/insertion_sort.py
@@ -1,13 +1,15 @@
-n = 5
-data = [8, 5, 4, 7, 2]
+import sys
+input = sys.stdin.readline
 
-''' Insertion Sort '''
-for i in range(1, n):
-    for j in range(i, 0, -1): # The variable j decreases from i to 1.
-        if data[j - 1] > data[j]: # Move forward.
-            data[j - 1], data[j] = data[j], data[j - 1] # Swap.
-        else: # When reaching a smaller value, then stop.
-            break
+def Sort(data):
+   for i in range(1, n):
+      for j in range(i, 0, -1): # The variable j decreases from i to 1.
+         if data[j - 1] > data[j]: # Move forward.
+               data[j - 1], data[j] = data[j], data[j - 1] # Swap.
+         else: # When reaching a smaller value, then stop.
+               break
 
-for x in data:
-    print(x)
+n = int(input())
+data = list(map(int, input().split()))
+Sort(data)
+print(data)
