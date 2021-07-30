e852d9a6952626116f8255369ed96475900b4d34
hanjiseong <55674646+tjdwl3269@users.noreply.github.com>
2021-07-30 15:57:32 +0900

    Update python_sort_library.py

--- Sorting/python_sort_library.py
+++ Sorting/python_sort_library.py
@@ -9,9 +9,18 @@ for x in data:
 
 print("---------------")
 
-''' Python Sort Library (Based on a key) '''
+''' Python Sort Library (Based on a key1) '''
 data = [(25, 'Na'), (20, 'Kim'), (23, 'Seo'), (28, 'Park'), (20, 'Ahn')]
-data.sort(key=lambda x: x[0]) # Stable Sort (When using a key)
+data.sort(key=lambda x: x[0]) # Stable Sort1 (When using a key)
+
+for x in data:
+    print(x)
+
+print("---------------")
+
+''' Python Sort Library (Based on a key2) '''
+data = [(25, 'Na'), (20, 'Kim'), (23, 'Seo'), (28, 'Park'), (20, 'Ahn')]
+data.sort(key=lambda x: (x[0], x[1])) # Stable Sort2 (When using a key)
 
 for x in data:
     print(x)
